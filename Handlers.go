9a5e6aa449cdbf5c5bbed5f7a74014f0e18dfe45
W100-KAUSHALS\Aspire5 <kaushal.shah@crestdatasys.com>
2018-08-18 15:13:26 +0530

    added post req

--- Handlers.go
+++ Handlers.go
@@ -2,18 +2,45 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 )
 
+var allUsers []string
+
 //Subscribe for subscribing new user
 func Subscribe(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
 	decoder := json.NewDecoder(r.Body)
 	var user User
 	err := decoder.Decode(&user)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(user.Username)
-	fmt.Fprintf(w, "Subscribed!")
+	if !userAlreadyExists(user.Username) {
+		allUsers = append(allUsers, user.Username)
+		userJSON, err := json.Marshal(allUsers)
+		if err != nil {
+			log.Fatal("Can't encode to JSON ", err)
+		} else {
+			w.Write(userJSON)
+		}
+	} else {
+		errorMsg, err := json.Marshal("User already exists.")
+		if err != nil {
+			log.Fatal("Can't encode to JSON ", err)
+		}
+		w.Write(errorMsg)
+	}
+}
+
+func userAlreadyExists(uname string) bool {
+	for _, username := range allUsers {
+		if username == uname {
+			return true
+		}
+	}
+	return false
 }
